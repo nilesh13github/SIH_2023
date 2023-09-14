1e9cde6cdc13715aabc6a0f1f3fc598e9eb215a6
nilesh13github <115298747+nilesh13github@users.noreply.github.com>
2023-09-14 22:19:06 +0530

    update app.py
    flask minimal app

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
 from flask import Flask, render_template, flash, redirect, url_for, request, session
 from flask_pymongo import PyMongo
 from flask_bcrypt import Bcrypt
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
