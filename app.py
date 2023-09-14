53be334d7e5268dcb73b52d9349a7c7fb2879a3c
Aditya Patil <adityapa37@gmail.com>
2023-09-14 22:21:51 +0530

    update app.py

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
 from flask import Flask, render_template, flash, redirect, url_for, request, session
 from flask_pymongo import PyMongo
 from flask_bcrypt import Bcrypt
-<<<<<<< HEAD
+from flask_wtf import FlaskForm
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
-=======
-from flask_wtf import FlaskForm
-
->>>>>>> 1dd4fdc9c23630898d96df86bfb7b6cc198f97ee
