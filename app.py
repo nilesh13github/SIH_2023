1dd4fdc9c23630898d96df86bfb7b6cc198f97ee
Aditya Patil <adityapa37@gmail.com>
2023-09-14 22:18:54 +0530

    update app.py

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
 from flask import Flask, render_template, flash, redirect, url_for, request, session
 from flask_pymongo import PyMongo
 from flask_bcrypt import Bcrypt
+from flask_wtf import FlaskForm
+
