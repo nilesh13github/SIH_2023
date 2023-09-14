67f06aa0789531d12f194b554250863de840ff0d
Aditya Patil <adityapa37@gmail.com>
2023-09-14 22:24:07 +0530

    update app.py

--- app.py
+++ app.py
@@ -4,6 +4,11 @@ from flask_bcrypt import Bcrypt
 from flask_wtf import FlaskForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/SIH'
+
+mongo = PyMongo(app)
+bcrypt = Bcrypt(app)
 
 @app.route('/')
 def hello_world():
