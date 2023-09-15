c29e4a1c63f4284f871d66294da5100e3a7872be
Aditya Patil <adityapa37@gmail.com>
2023-09-15 11:01:53 +0530

    update

--- app.py
+++ app.py
@@ -3,16 +3,21 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, validators
 from pymongo import MongoClient
 from flask_bcrypt import Bcrypt
+from email_validator import validate_email, EmailNotValidError  # Import email_validator
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your_secret_key'  
+app.config['SECRET_KEY'] = 'aditya'
+bcrypt = Bcrypt(app)
 client = MongoClient('mongodb://localhost:27017/')
-db = client['mydb']
+db = client['edvocate']
 users = db['users']
 
 class RegistrationForm(FlaskForm):
     fullname = StringField('Full Name', validators=[validators.InputRequired()])
-    email = StringField('Email', validators=[validators.InputRequired(), validators.Email()])
+    email = StringField('Email', validators=[
+        validators.InputRequired(),
+        validators.Email(message='Invalid email address'),  # Basic email validation
+    ])
     phone = StringField('Phone Number', validators=[validators.InputRequired()])
     aadhar_number = StringField('Aadhar Number', validators=[validators.InputRequired()])
     password = PasswordField('Password', validators=[
@@ -29,22 +34,26 @@ class RegistrationForm(FlaskForm):
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-     form = RegistrationForm()
+    form = RegistrationForm()
 
-     if form.validate_on_submit():
-        password_hash = Bcrypt.generate_password_hash(form.password.data).decode('utf-8')  # Hash the password
-        user_data = {
-            'fullname': form.fullname.data,
-            'email': form.email.data,
-            'phone': form.phone.data,
-            'aadhar_number': form.aadhar_number.data,
-            'password': password_hash,  # Store the hashed password
-        }
-        users.insert_one(user_data)
-        flash('Registration successful. Please log in.', 'success')
-        return redirect(url_for('login'))
+    if form.validate_on_submit():
+        try:
+            validate_email(form.email.data)  # Validate email using email_validator
+            password_hash = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+            user_data = {
+                'fullname': form.fullname.data,
+                'email': form.email.data,
+                'phone': form.phone.data,
+                'aadhar_number': form.aadhar_number.data,
+                'password': password_hash,
+            }
+            users.insert_one(user_data)
+            flash('Registration successful. Please log in.', 'success')
+            return redirect(url_for('login'))
+        except EmailNotValidError as e:
+            flash('Invalid email address.', 'danger')
 
-     return render_template('register.html', form=form)
+    return render_template('register.html', form=form)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
